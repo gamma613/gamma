727599eed1612cd626af942e677321c6e0f12583
Steven McCoy <7.mccoy@gmail.com>
2026-03-14 14:47:12 -0400

    Vertically center header row

--- src/modules/header/components/Header.tsx
+++ src/modules/header/components/Header.tsx
@@ -13,32 +13,34 @@ export function Header() {
 
   return (
     <header
-      className="fixed inset-x-0 bottom-0 z-30 bg-background/30 h-[var(--header-h)] supports-[backdrop-filter]:backdrop-blur-md"
+      className="fixed inset-x-0 bottom-0 z-30 bg-background/30 supports-[backdrop-filter]:backdrop-blur-md"
       style={{ height: '100px' }}
     >
-      <div className="mx-auto max-w-md sm:max-w-3xl px-3">
-        <div className="flex items-center">
-          <Link href="/" className="inline-block">
-            <Image src="/logo-transparent.png" alt="gamma logo" height="44" width="44" priority />
-          </Link>
+      <div className="mx-auto max-w-md sm:max-w-3xl px-3 h-full flex flex-col">
+        <div className="flex-1 flex items-center">
+          <div className="flex w-full items-center">
+            <Link href="/" className="inline-block">
+              <Image src="/logo-transparent.png" alt="gamma logo" height="44" width="44" priority />
+            </Link>
 
-          <div className="flex flex-1 px-3 justify-center">
-            <HeaderPlayer />
+            <div className="flex flex-1 px-3 justify-center">
+              <HeaderPlayer />
+            </div>
+
+            <Button
+              type="button"
+              variant="ghost"
+              className="h-11 w-11 p-0 md:h-9 md:w-9"
+              aria-label="Navigate"
+              title="Navigate"
+              onClick={() => setNavOpen(true)}
+            >
+              <Menu className="size-5" />
+            </Button>
           </div>
-
-          <Button
-            type="button"
-            variant="ghost"
-            className="h-11 w-11 p-0 md:h-9 md:w-9"
-            aria-label="Navigate"
-            title="Navigate"
-            onClick={() => setNavOpen(true)}
-          >
-            <Menu className="size-5" />
-          </Button>
         </div>
 
-        <SeekBar />
+        <SeekBar className="mt-auto" />
       </div>
     </header>
   );
