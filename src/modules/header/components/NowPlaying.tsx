cb37229928e1a09e076c0410939eb41b0403d50e
Steven McCoy <7.mccoy@gmail.com>
2026-03-16 00:59:24 -0400

    Reorder Now Playing widgets, add a11y, and move seek bar in

--- src/modules/header/components/NowPlaying.tsx
+++ src/modules/header/components/NowPlaying.tsx
@@ -1,4 +1,4 @@
-import { MuteButton, Player, PlayToggleButton, TrackArt, TrackArtist, TrackTitle, VolumeSlider } from '@/modules/player';
+import { Player, PlayToggleButton, SeekBar, TrackArt, TrackArtist, TrackTitle, VolumeSlider } from '@/modules/player';
 //
 import { TrackDuration } from '@/modules/player/components/TrackDuration';
 import { TrackPosition } from '@/modules/player/components/TrackPosition';
@@ -7,19 +7,20 @@ import { TrackPosition } from '@/modules/player/components/TrackPosition';
 
 export function NowPlaying() {
   return (
-    <>
+    <div aria-label="Now playing" role="Region">
+      {/* Embed the player (hidden) */}
       <div aria-hidden="true" className="hidden">
         <Player />
       </div>
-      <div className="flex items-center">
-        
+
+      {/* Toolbar */}
+      <div role="toolbar" aria-label="Media controls" className="flex flex-row items-center">
         <TrackArt width={50} height={50} />
-        
         <PlayToggleButton /> 
-        <MuteButton /> 
+        {/* TODO: move this into a button w/ popover */}
         <VolumeSlider className="px-6 flex-1 max-w-[150px]" />
-        <div className="ml-auto flex flex-col items-start">
-          <TrackArtist className="w-full max-w-[12rem] sm:max-w-[16rem] truncate text-xs leading-none text-muted-foreground" />
+        <div className="">
+          <TrackArtist aria-labelledby="now-playing-title" className="w-full max-w-[12rem] sm:max-w-[16rem] truncate text-xs leading-none text-muted-foreground" />
           <TrackTitle className="w-full max-w-[12rem] sm:max-w-[16rem] truncate text-xs leading-none text-muted-foreground" />
           <div className="flex items-center gap-2 tabular-nums text-xs leading-none text-muted-foreground">
             <TrackPosition className="text-foreground" />
@@ -27,7 +28,11 @@ export function NowPlaying() {
             <TrackDuration className="text-foreground" />
           </div>
         </div>
+        {/* Sits overtop of the header */}
+        <SeekBar className="fixed inset-x-0 bottom-(--header-height) z-31" />
       </div>
-    </>
+
+      
+    </div>
   );
 }
